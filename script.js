14c2aae8bc9004e7c61d8af0a000561d35036cc2
Akimitsu <emailjasonpf@gmail.com>
2024-07-03 00:38:14 +0200

    Added functions to convert between JSON and JSO and example Data

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var allProjects = []
+var currentProject = {}
 var pages = [
     document.getElementById("homeScreen"),
     document.getElementById("createNewProject"),
@@ -5,6 +7,11 @@ var pages = [
     document.getElementById("settings"),
     document.getElementById("about")
 ]
+
+/**
+ * 
+ * @param {Number} page 
+ */
 
 function openPage(page) {
     pages.forEach(currentPage => {
@@ -17,6 +24,11 @@ function openPage(page) {
     if (page == 1) newProjectName.focus()
 }
 
+/**
+ * 
+ * @param {String} name 
+ */
+
 function newProjectNameOnInput(name) {
     if (name.value == '') {
         document.getElementById('pleaseEnterNamePrompt').style.color = 'rgb(241, 76, 76)'
@@ -25,6 +37,87 @@ function newProjectNameOnInput(name) {
         document.getElementById('pleaseEnterNamePrompt').style.color = 'transparent'
         document.getElementById('createNewProjectButton').classList.remove('blocked')
     }
+}
+
+/**
+ * 
+ * @param {String} name 
+ * @param {[{}]} otherProjects 
+ */
+
+function createNewProject(name, otherProjects) {
+    let newProject = {
+        name: name,
+        chips: [
+            {
+                name: "AND",
+                usesCode: true,
+                inputPins: [{
+                    id: 0,
+                    name: "PIN 1",
+                    color: "Red"
+                },
+                {
+                    id: 1,
+                    name: "PIN 2",
+                    color: "Red"
+                }],
+                outputPins: [{
+                    id: 0,
+                    name: "PIN 3",
+                    color: "Red"
+                }],
+                code: () => {
+                    this.outputPins[0].state = this.inputPins[0].state && this.inputPins[1].state
+                }
+            },
+            {
+                name: "NOT",
+                usesCode: true,
+                inputPins: [{
+                    id: 0,
+                    name: "PIN 1",
+                    color: "Red"
+                }],
+                outputPins: [{
+                    id: 0,
+                    name: "PIN 2",
+                    color: "Red"
+                }],
+                code: () => {
+                    this.outputPins[0].state = !this.inputPins[0].state
+                }
+            }
+        ],
+        starredChips: ["AND", "NOT"]
+    }
+    convertChipCodesToString(newProject)
+    otherProjects.push(newProject)
+    localStorage.projects = JSON.stringify(otherProjects)
+}
+
+/**
+ * 
+ * @param {{}} project 
+ */
+
+function convertChipCodesToString(project) {
+    project.chips.forEach(chip => {
+        if (!chip.usesCode) return
+        chip.code = chip.code.toString()
+    })
+}
+
+/**
+ * 
+ * @param {{}} project 
+ */
+
+function convertChipCodesToFuncs(project) {
+    project.chips.forEach(chip => {
+        if (!chip.usesCode) return
+        chip.code = eval(chip.code)
+    })
 }
 
 openPage(0)
